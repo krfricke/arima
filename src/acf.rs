33523c1ca2b3b4f1df32aa65b0e27a810f2537db
Kai Fricke <coding@kaifricke.com>
2019-10-19 16:48:12 +0200

    added blas libraries as features

--- src/acf.rs
+++ src/acf.rs
@@ -1,4 +1,15 @@
-extern crate openblas_src;
+#[cfg(feature = "accelerate")]
+extern crate accelerate_src as raw;
+
+#[cfg(feature = "intel-mkl")]
+extern crate intel_mkl_src as raw;
+
+#[cfg(feature = "netlib")]
+extern crate netlib_src as raw;
+
+#[cfg(feature = "openblas")]
+extern crate openblas_src as raw;
+
 extern crate lapack_sys;
 extern crate lapack;
 
